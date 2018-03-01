72b82ca77070b1bf6425c975a1713db6587b0c92
Kevin Custer <kecuster@gmail.com>
2018-03-01 09:41:08 -0500

    Working on transaction type

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,9 @@
 class Account < ApplicationRecord
 	belongs_to :user
 	has_many :transactions, dependent: :destroy
+	after_create :create_initial_transaction
+
+	def create_initial_transaction
+		Transaction.create(trx_date: DateTime.now, account_id: self.id, description: "Starting Balance", amount: self.current_balance)
+	end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,25 @@
 class Transaction < ApplicationRecord
 	belongs_to :account
+	after_create :update_account_balance_new
+	after_update :update_account_balance_edit
+	after_destroy :update_account_balance_destroy
+
+	attr_accessor :trx_type
+	
+	def update_account_balance_new
+		@account = Account.find(account_id)
+		@account.update_attributes(current_balance: @account.current_balance - amount)
+	end
+
+	def update_account_balance_edit
+		@account = Account.find(account_id)
+		if amount_changed?
+			@account.update_attributes(current_balance: @account.current_balance + amount_was - amount)
+		end
+	end
+
+	def update_account_balance_destroy
+		@account = Account.find(account_id)
+		@account.update_attributes(current_balance: @account.current_balance + amount_was)
+	end
 end
