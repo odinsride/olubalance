2c9efef6cec9304e8e705e4f977bb31b96479c78
Kevin Custer <kecuster@gmail.com>
2018-03-14 15:26:55 -0400

    Changes to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,8 @@ class Account < ApplicationRecord
 	after_create :create_initial_transaction
 
 	def create_initial_transaction
+		@initbalance = self.current_balance
 		Transaction.create(trx_date: DateTime.now, account_id: self.id, description: "Starting Balance", amount: self.current_balance)
+		self.update_attributes(current_balance: @initbalance)
 	end
 end
