c58680cb0fcd24fe4ea711872858f74677b85ac3
Kevin Custer <kecuster@gmail.com>
2018-07-05 13:28:46 -0400

    Fixed issue with account form, working on navbar mobile menu

--- app/assets/javascripts/ob-materialize.js
+++ app/assets/javascripts/ob-materialize.js
@@ -2,3 +2,9 @@ $(document).on('turbolinks:load', function() {
   Materialize.Modal._count = 0;
   $('.modal').modal();
 });
+
+$(document).on('turbolinks:load', function() {
+  $(".dropdown-button").dropdown({ hover: true });
+  $(".button-collapse").sideNav();
+});
+
