32343690290d5f22b584042520b0bc74d2f501c3
Kevin Custer <kecuster@gmail.com>
2019-05-19 20:23:35 -0400

    Removed lib/constants.rb and moved constants to model files. Disabled autoload_path config option.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,11 @@
 # An account which will store many transactions and belongs to one user
 class Account < ApplicationRecord
+  # Define Constants
+  NO_ACCOUNT_DESC = "It looks like you don't have any accounts added. To add an account, \
+                     click the add account button at the top of the page :)".freeze
+
+  NO_INACTIVE_DESC = 'You have no inactive accounts :)'.freeze
+
   belongs_to :user
   has_many :transactions, dependent: :destroy
   has_many :documents
--- lib/constants.rb
+++
@@ -1,7 +0,0 @@
-module Constants
-  # Define Constants
-  NO_ACCOUNT_DESC = "It looks like you don't have any accounts added. To add an account, \
-                     click the add account button at the top of the page :)".freeze
-
-  NO_INACTIVE_DESC = 'You have no inactive accounts :)'.freeze
-end
