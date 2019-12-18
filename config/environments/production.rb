a39f1ff94ff8a24c67bca019eab22de8e6bb48b0
Kevin Custer <kecuster@gmail.com>
2019-12-17 22:02:29 -0500

    Update relative_url_root for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
    config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
+  config.action_controller.relative_url_root = "/olubalance"
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
