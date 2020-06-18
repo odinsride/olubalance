6fec0e01819a134390076628feeeb39956101607
Kevin Custer <kecuster@gmail.com>
2020-06-17 21:14:12 -0400

    Added foreman config. Account modals working

--- app/javascript/controllers/shared_controller.js
+++ app/javascript/controllers/shared_controller.js
@@ -3,9 +3,11 @@ import * as bulmaToast from 'bulma-toast'
 
 export default class extends Controller {
 
+  static targets = [ "modal" ]
+
   connect () {
     //Invoke bulma toast notifications, if any
-    //this.element.remove()
+    //this.element.remove() TODO: is this needed?
     var message = this.data.get("message")
     var messageType = this.data.get("message-type")
     if (message && messageType) {
@@ -16,5 +18,11 @@ export default class extends Controller {
         duration: 1500 
       })
     }
+  }
+
+  toggleModal (e) {
+    // console.log(e.currentTarget.dataset.id)
+    let modalId = e.currentTarget.dataset.id
+    document.getElementById(modalId).classList.toggle('is-active')
   }
 }
