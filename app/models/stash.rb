96f08fa6d7aa220ec84844e0895dcad0f9b130f3
Kevin Custer <kecuster@gmail.com>
2020-06-21 12:31:38 -0400

    Correct bug where deleting a stash doesn't return the stashed money to the account. Closes #99

--- app/models/stash.rb
+++ app/models/stash.rb
@@ -13,4 +13,22 @@ class Stash < ApplicationRecord
                    numericality: { greater_than_or_equal_to: :balance }
 
   validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
+  before_destroy :unstash
+
+
+  private
+
+  # Create an account transaction to return any stashed money
+  # to the Account balance when a Stash is deleted
+  def unstash
+    transaction = Transaction.new
+    transaction.trx_type = 'credit'
+    transaction.trx_date = Time.current
+    transaction.account_id = account_id
+    transaction.description = 'Transfer from ' + name + ' (Stash Deleted)'
+    transaction.amount = balance.abs
+    transaction.locked = true
+    transaction.save
+  end
 end
