fba012aa9327a7e387424a82b39ad474c1c71ffa
Kevin Custer <kecuster@gmail.com>
2020-08-10 20:36:04 -0400

    Devise mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   
   config.active_storage.service = :amazondev
