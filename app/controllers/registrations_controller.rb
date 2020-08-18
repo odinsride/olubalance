a3ecd79a04a40b429b79622ba46400d415a5d368
Kevin Custer <kecuster@gmail.com>
2020-08-18 18:14:49 -0400

    Switch to reCAPTCHA v2. Closes #114

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,22 +6,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def check_captcha
-    success = verify_recaptcha(action: 'registration', minimum_score: 0.5)
-    checkbox_success = verify_recaptcha unless success
-    if success || checkbox_success
-
-    else
-      if !success
-        @show_checkbox_recaptcha = true
-      end
-      render 'new'
+    unless verify_recaptcha
+      self.resource = resource_class.new sign_up_params
+      resource.validate # Look for any other validation errors besides reCAPTCHA
+      set_minimum_password_length
+      respond_with_navigational(resource) { render :new }
     end
-
-    # unless verify_recaptcha
-    #   self.resource = resource_class.new sign_up_params
-    #   resource.validate # Look for any other validation errors besides reCAPTCHA
-    #   set_minimum_password_length
-    #   respond_with_navigational(resource) { render :new }
-    # end
   end
 end
