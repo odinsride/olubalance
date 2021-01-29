203f5f5300b8620edf83e97577017fbacbb51fbe
Kevin Custer <kecuster@gmail.com>
2021-01-29 06:10:10 -0500

    ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :set_timezone
-
-  before_action :assign_navbar_content
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def current_user
-    UserDecorator.decorate(super) unless super.nil?
-  end
-
-  def assign_navbar_content
-    @navbar_accounts = current_user.accounts if user_signed_in?
-  end
 
   protected
 
@@ -22,12 +10,5 @@ class ApplicationController < ActionController::Base
       { |u| u.permit(:first_name, :last_name, :timezone, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) \
       { |u| u.permit(:first_name, :last_name, :timezone, :email, :password, :current_password, :password_confirmation) }
-  end
-
-  private
-
-  def set_timezone
-    tz = current_user ? current_user.timezone : nil
-    Time.zone = tz || ActiveSupport::TimeZone['UTC']
   end
 end
