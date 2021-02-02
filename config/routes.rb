70dcab9df541d9036199c3e3aaa0bc1eec3d6703
Kevin Custer <kecuster@gmail.com>
2021-02-01 19:52:05 -0500

    Add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users, controllers: { registrations: 'registrations' }
 
   get 'accounts/inactive' => 'accounts#inactive'
--- spec/decorators/transfer_decorator_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe TransferDecorator do
-end
